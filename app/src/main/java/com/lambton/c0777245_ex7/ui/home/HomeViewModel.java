d824664ddd0472ab9b3318243e36859f01c8b717
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-07 21:02:03 -0400

    Initial commit

---
+++ app/src/main/java/com/lambton/c0777245_ex7/ui/home/HomeViewModel.java
@@ -0,0 +1,20 @@
+package com.lambton.c0777245_ex7.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+    private String homeText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+//        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
